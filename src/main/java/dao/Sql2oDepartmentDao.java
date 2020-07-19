0398fc5419fa046fe2b7f1ca8669b90a3d4edf65
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-19 11:59:09 +0300

    Add methods for deleting data from database in Sql2oDepartmentDao

--- src/main/java/dao/Sql2oDepartmentDao.java
+++ src/main/java/dao/Sql2oDepartmentDao.java
@@ -59,11 +59,20 @@ public class Sql2oDepartmentDao implements DepartmentDao {
 
     @Override
     public void deleteById(int id) {
-
+        String sql = "DELETE FROM departments WHERE id = :id ;";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
     }
 
     @Override
     public void clearAll() {
-
+        String sql = "DELETE FROM departments;";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .executeUpdate();
+        }
     }
 }
