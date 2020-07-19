680cf1509c10a2177032c254c4c17cae8b0d33ae
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-19 10:58:16 +0300

    Import gradle dependencies in build.gradle, Add Sql2oDepartmentDao class together with its properties and implement methods

---
+++ src/main/java/dao/Sql2oDepartmentDao.java
@@ -0,0 +1,44 @@
+package dao;
+
+import models.Department;
+import org.sql2o.*;
+
+import java.util.List;
+
+public class Sql2oDepartmentDao implements DepartmentDao {
+    private final Sql2o sql2o;
+
+    public Sql2oDepartmentDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Department department) {
+
+    }
+
+    @Override
+    public List<Department> getAll() {
+        return null;
+    }
+
+    @Override
+    public Department findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, String name, String description, int noOfEmployees) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
+}
