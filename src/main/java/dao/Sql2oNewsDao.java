8a0502cb055d81bf5722519ed8c250992f09586d
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-19 19:23:53 +0300

    Add tests for Sql2oNewsDaoTest and add throw on mapping failure to Sql2oNewsDao class

--- src/main/java/dao/Sql2oNewsDao.java
+++ src/main/java/dao/Sql2oNewsDao.java
@@ -33,6 +33,7 @@ public class Sql2oNewsDao implements NewsDao {
         String sql = "SELECT * FROM news;";
         try (Connection con = sql2o.open()) {
             return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
                     .executeAndFetch(News.class);
         }
     }
@@ -43,6 +44,7 @@ public class Sql2oNewsDao implements NewsDao {
         try (Connection con = sql2o.open()) {
             return con.createQuery(sql)
                     .addParameter("id", id)
+                    .throwOnMappingFailure(false)
                     .executeAndFetchFirst(News.class);
         }
     }
--- src/test/java/dao/Sql2oNewsDaoTest.java
+++ src/test/java/dao/Sql2oNewsDaoTest.java
@@ -1,10 +1,15 @@
 package dao;
 
+import models.News;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class Sql2oNewsDaoTest {
     private static Sql2oNewsDao newsDao;
@@ -28,5 +33,75 @@ public class Sql2oNewsDaoTest {
     public static void shutDown() throws Exception{
         conn.close();
         System.out.println("Connection closed");
+    }
+
+    @Test
+    public void addingNewsSetsId() {
+        News news = setUpNews();
+        int originalNewsId = news.getId();
+        newsDao.add(news);
+        assertNotEquals(originalNewsId, news.getId());
+    }
+
+    @Test
+    public void add_individualNewsCanBeFoundById() {
+        News news = setUpNews();
+        newsDao.add(news);
+        News foundNews = newsDao.findById(news.getId());
+        assertEquals(news, foundNews);
+    }
+
+    @Test
+    public void findById_individualNewsCanBeFoundById() {
+        News news = setUpNews();
+        newsDao.add(news);
+        News foundNews = newsDao.findById(news.getId());
+        assertEquals(news, foundNews);
+    }
+
+    @Test
+    public void getAll_allNewsAreReturnedCorrectly() {
+        News news = setUpNews();
+        newsDao.add(news);
+        assertEquals(1, newsDao.getAll().size());
+    }
+
+    @Test
+    public void getAll_nothingIsReturnedFromAnEmptyDatabase() {
+        assertEquals(0, newsDao.getAll().size());
+    }
+
+    @Test
+    public void update_newsIsUpdatedCorrectly() {
+        News news = setUpNews();
+        newsDao.add(news);
+        int currentId = news.getId();
+        newsDao.update(currentId, "maths", "maths subjects");
+        News updatedNews = newsDao.findById(currentId);
+        assertNotEquals(news, updatedNews);
+    }
+
+    @Test
+    public void deleteById_individualNewsIsDeletedCorrectlyByItsId() {
+        News news = setUpNews();
+        News otherNews = new News("Schools to open","Mutali");
+        newsDao.add(news);
+        newsDao.add(otherNews);
+        newsDao.deleteById(news.getId());
+        assertEquals(1, newsDao.getAll().size());
+    }
+
+    @Test
+    public void clearAll_allAddedNewsCanBeCleared() {
+        News news = setUpNews();
+        News otherNews = new News("Schools to open","Mutali");
+        newsDao.add(news);
+        newsDao.add(otherNews);
+        newsDao.clearAll();
+        assertEquals(0, newsDao.getAll().size());
+    }
+
+    public News setUpNews(){
+        return new News("Schools to close", "M Kitavi");
     }
 }
