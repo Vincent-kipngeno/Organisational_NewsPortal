55e9ed68a02f94dbc584da63d5cc24e513128774
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-19 16:08:10 +0300

    Import postgress dependency, and add tests for the Sql2oDepartmentDao class methods

--- src/test/java/dao/Sql2oDepartmentDaoTest.java
+++ src/test/java/dao/Sql2oDepartmentDaoTest.java
@@ -1,7 +1,11 @@
 package dao;
 
+import models.Department;
 import org.junit.*;
 import org.sql2o.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class Sql2oDepartmentDaoTest {
     private static Sql2oDepartmentDao departmentDao;
@@ -12,7 +16,7 @@ public class Sql2oDepartmentDaoTest {
         String connectionString = "jdbc:postgresql://localhost:5432/news_portal_test";
         Sql2o sql2o = new Sql2o(connectionString, "vincent", "Taptet#2001");
         departmentDao = new Sql2oDepartmentDao(sql2o);
-        conn = sql2o.open();
+        conn = (Connection) sql2o.open();
     }
 
     @After
@@ -28,26 +32,72 @@ public class Sql2oDepartmentDaoTest {
     }
 
     @Test
-    public void add() {
+    public void addingDepartmentsSetsId() {
+        Department department = setNewDepartment();
+        int originalDepartmentId = department.getId();
+        departmentDao.add(department);
+        assertNotEquals(originalDepartmentId, department.getId());
     }
 
     @Test
-    public void getAll() {
+    public void add_individualDepartmentsCanBeFoundById() {
+        Department department = setNewDepartment();
+        departmentDao.add(department);
+        Department foundDepartment = departmentDao.findById(department.getId());
+        assertEquals(department, foundDepartment);
     }
 
     @Test
-    public void findById() {
+    public void findById_individualDepartmentCanBeFoundById() {
+        Department department = setNewDepartment();
+        departmentDao.add(department);
+        Department foundDepartment = departmentDao.findById(department.getId());
+        assertEquals(department, foundDepartment);
     }
 
     @Test
-    public void update() {
+    public void getAll_allDepartmentAreReturnedCorrectly() {
+        Department department = setNewDepartment();
+        departmentDao.add(department);
+        assertEquals(1, departmentDao.getAll().size());
     }
 
     @Test
-    public void deleteById() {
+    public void getAll_nothingIsReturnedFromAnEmptyDatabase() {
+        assertEquals(0, departmentDao.getAll().size());
     }
 
     @Test
-    public void clearAll() {
+    public void update_departmentIsUpdatedCorrectly() {
+        Department department = setNewDepartment();
+        departmentDao.add(department);
+        int currentId = department.getId();
+        departmentDao.update(currentId, "maths", "maths subjects", 2);
+        Department updatedDepartment = departmentDao.findById(currentId);
+        assertNotEquals(department, updatedDepartment);
+    }
+
+    @Test
+    public void deleteById_individualDepartmentIsDeletedCorrectlyByItsId() {
+        Department department = setNewDepartment();
+        Department otherDepartment = new Department("Maths","maths subjects", 1);
+        departmentDao.add(department);
+        departmentDao.add(otherDepartment);
+        departmentDao.deleteById(department.getId());
+        assertEquals(1, departmentDao.getAll().size());
+    }
+
+    @Test
+    public void clearAll_allAddedDepartmentCanBeCleared() {
+        Department department = setNewDepartment();
+        Department otherDepartment = new Department("Maths","Maths subjects", 1);
+        departmentDao.add(department);
+        departmentDao.add(otherDepartment);
+        departmentDao.clearAll();
+        assertEquals(0, departmentDao.getAll().size());
+    }
+
+    public Department setNewDepartment(){
+        return new Department("Science", "science subjects", 5);
     }
 }
