174868c8c39277ff65289737dba9232f47e8861b
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-20 16:36:58 +0300

    Add declarations of Sql2oDao classes and connection string to connect to database and comments of routings to create and filters of exceptions and code to execute after every route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,72 @@
+import Exceptions.ApiException;
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentDao;
+import dao.Sql2oDepartmentNewsDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUserDao;
+import org.sql2o.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.after;
+import static spark.Spark.exception;
+
 public class App {
     public static void main (String[] args) {
+        Sql2oDepartmentNewsDao departmentNewsDao;
+        Sql2oNewsDao newsDao;
+        Sql2oUserDao userDao;
+        Sql2oDepartmentDao departmentDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:postgresql://localhost:5432/news_portal";
+        Sql2o sql2o = new Sql2o(connectionString, "vincent", "Taptet#2001");
+
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        departmentNewsDao = new Sql2oDepartmentNewsDao(sql2o);
+        userDao = new Sql2oUserDao(sql2o);
+
+        conn = sql2o.open();
+
+        //Post: Create new Department
+
+        //get: view all departments
+
+        //get: view individual department
+
+        //Post: Create new user
+
+        //get: view all users
+
+        //get: view individual user
+
+        //get: view users from a specific department and news related to them
+
+        //post: create news
+
+        //get: view all general news
+
+        //post: create department news
+
+
+        //FILTERS
+        exception(ApiException.class, (exception, req, res) -> {
+            ApiException err = exception;
+            Map<String, Object> jsonMap = new HashMap<>();
+            jsonMap.put("status", err.getStatusCode());
+            jsonMap.put("errorMessage", err.getMessage());
+            res.type("application/json");
+            res.status(err.getStatusCode());
+            res.body(gson.toJson(jsonMap));
+        });
+
+
+        after((req, res) ->{
+            res.type("application/json");
+        });
 
     }
 }
