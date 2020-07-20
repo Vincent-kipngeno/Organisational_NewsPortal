9f937808e6b3a2c488071900fa4920ba75135398
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-20 10:04:37 +0300

    Add test methods for Sql2oUserDao class

--- src/test/java/dao/Sql2oUserDaoTest.java
+++ src/test/java/dao/Sql2oUserDaoTest.java
@@ -1,12 +1,15 @@
 package dao;
 
+import models.User;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
+
 
 public class Sql2oUserDaoTest {
     private static Sql2oUserDao userDao;
@@ -32,4 +35,73 @@ public class Sql2oUserDaoTest {
         System.out.println("Connection closed");
     }
 
+    @Test
+    public void addingUserSetsId() {
+        User user = setUpUser();
+        int originalUserId = user.getId();
+        userDao.add(user);
+        assertNotEquals(originalUserId, user.getId());
+    }
+
+    @Test
+    public void add_individualUserCanBeFoundById() {
+        User user = setUpUser();
+        userDao.add(user);
+        User foundUser = userDao.findById(user.getId());
+        assertEquals(user, foundUser);
+    }
+
+    @Test
+    public void findById_individualUserCanBeFoundById() {
+        User user = setUpUser();
+        userDao.add(user);
+        User foundUser = userDao.findById(user.getId());
+        assertEquals(user, foundUser);
+    }
+
+    @Test
+    public void getAll_allUserAreReturnedCorrectly() {
+        User user = setUpUser();
+        userDao.add(user);
+        assertEquals(1, userDao.getAll().size());
+    }
+
+    @Test
+    public void getAll_nothingIsReturnedFromAnEmptyDatabase() {
+        assertEquals(0, userDao.getAll().size());
+    }
+
+    @Test
+    public void update_userIsUpdatedCorrectly() {
+        User user = setUpUser();
+        userDao.add(user);
+        int currentId = user.getId();
+        userDao.update(currentId, "Mutali", "principal", 1);
+        User updatedUser = userDao.findById(currentId);
+        assertNotEquals(user, updatedUser);
+    }
+
+    @Test
+    public void deleteById_individualUserIsDeletedCorrectlyByItsId() {
+        User user = setUpUser();
+        User otherUser = new User("Mutali", "principal", 1);
+        userDao.add(user);
+        userDao.add(otherUser);
+        userDao.deleteById(user.getId());
+        assertEquals(1, userDao.getAll().size());
+    }
+
+    @Test
+    public void clearAll_allAddedUserCanBeCleared() {
+        User user = setUpUser();
+        User otherUser = new User("Mutali", "principal", 2);
+        userDao.add(user);
+        userDao.add(otherUser);
+        userDao.clearAll();
+        assertEquals(0, userDao.getAll().size());
+    }
+
+    public User setUpUser(){
+        return new User("Mkitavi", "Teacher", 1);
+    }
 }
