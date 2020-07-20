297b42bbb22089f3f84bef4c3500046b99f57f49
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-20 10:46:11 +0300

    Add test methods for testing the Sql2oDepartmentNewsDao class methods

--- src/test/java/dao/Sql2oDepartmentNewsDaoTest.java
+++ src/test/java/dao/Sql2oDepartmentNewsDaoTest.java
@@ -1,8 +1,10 @@
 package dao;
 
+import models.DepartmentNews;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
@@ -30,5 +32,75 @@ public class Sql2oDepartmentNewsDaoTest {
     public static void shutDown() throws Exception{
         conn.close();
         System.out.println("Connection closed");
+    }
+
+    @Test
+    public void addingNewsSetsId() {
+        DepartmentNews news = setUpNews();
+        int originalNewsId = news.getId();
+        departmentNewsDao.add(news);
+        assertNotEquals(originalNewsId, news.getId());
+    }
+
+    @Test
+    public void add_individualNewsCanBeFoundById() {
+        DepartmentNews news = setUpNews();
+        departmentNewsDao.add(news);
+        DepartmentNews foundNews = departmentNewsDao.findById(news.getId());
+        assertEquals(news, foundNews);
+    }
+
+    @Test
+    public void findById_individualNewsCanBeFoundById() {
+        DepartmentNews news = setUpNews();
+        departmentNewsDao.add(news);
+        DepartmentNews foundNews = departmentNewsDao.findById(news.getId());
+        assertEquals(news, foundNews);
+    }
+
+    @Test
+    public void getAll_allNewsAreReturnedCorrectly() {
+        DepartmentNews news = setUpNews();
+        departmentNewsDao.add(news);
+        assertEquals(1, departmentNewsDao.getAll().size());
+    }
+
+    @Test
+    public void getAll_nothingIsReturnedFromAnEmptyDatabase() {
+        assertEquals(0, departmentNewsDao.getAll().size());
+    }
+
+    @Test
+    public void update_newsIsUpdatedCorrectly() {
+        DepartmentNews news = setUpNews();
+        departmentNewsDao.add(news);
+        int currentId = news.getId();
+        departmentNewsDao.update(currentId, "maths", "maths subjects", 2);
+        DepartmentNews updatedNews = departmentNewsDao.findById(currentId);
+        assertNotEquals(news, updatedNews);
+    }
+
+    @Test
+    public void deleteById_individualNewsIsDeletedCorrectlyByItsId() {
+        DepartmentNews news = setUpNews();
+        DepartmentNews otherNews = new DepartmentNews("Schools to open","Mutali", 1);
+        departmentNewsDao.add(news);
+        departmentNewsDao.add(otherNews);
+        departmentNewsDao.deleteById(news.getId());
+        assertEquals(1, departmentNewsDao.getAll().size());
+    }
+
+    @Test
+    public void clearAll_allAddedNewsCanBeCleared() {
+        DepartmentNews news = setUpNews();
+        DepartmentNews otherNews = new DepartmentNews("Schools to open","Mutali", 1);
+        departmentNewsDao.add(news);
+        departmentNewsDao.add(otherNews);
+        departmentNewsDao.clearAll();
+        assertEquals(0, departmentNewsDao.getAll().size());
+    }
+
+    public DepartmentNews setUpNews(){
+        return new DepartmentNews("Schools to close", "M Kitavi", 1);
     }
 }
